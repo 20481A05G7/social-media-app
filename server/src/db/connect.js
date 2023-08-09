3768c23c9b818f7057c088958b107d795f2d3030
20481A05G7 <99860316+20481A05G7@users.noreply.github.com>
2023-08-09 19:19:10 +0530

    Update connect.js

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // Middleware
-const db = 'mongodb+srv://socialmedia:socialmedia@cluster0.nsffmg4.mongodb.net/SocialMediaApp?retryWrites=true&w=majority'
+const db = 'mongodb+srv://narendramullangi:Narendra1122@cluster0.ioqcd9a.mongodb.net/?retryWrites=true&w=majority'
 // Connect to MongoDB using the connection string
 mongoose.connect(db, {
   useNewUrlParser: true,
